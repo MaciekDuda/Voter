815e563e970b88f3df603c54b6388186ddf5fbb6
MaciekDuda <maciejduda1626@gmail.com>
2024-07-05 16:08:41 +0200

    sql

--- savevotes.php
+++ savevotes.php
@@ -1,21 +1,10 @@
 <?php
 
-// Odczyt danych z żądania POST
-$request_body = file_get_contents('php://input');
-$data = json_decode($request_body);
-
-// Dane do zapisu
-$name = $data->name;
-$votes = $data->votes; // Tablica z wyborami
-
-// Tutaj można dodać kod do zapisania danych w bazie danych
-// Na przykład, przy użyciu biblioteki do obsługi bazy danych
-
-// Przykładowe połączenie z bazą danych i zapis danych
+// Połączenie z bazą danych
 $servername = "localhost";
 $username = "dudzior";
-$password = "password";
-$dbname = "ankieta_db";
+$password = ""; // Ustaw swoje hasło
+$dbname = "ankieta_db"; // Ustaw nazwę swojej bazy danych
 
 // Tworzenie połączenia
 $conn = new mysqli($servername, $username, $password, $dbname);
@@ -25,14 +14,20 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
+// Odczyt danych z żądania POST
+$data = json_decode(file_get_contents('php://input'), true);
+
+$name = $data['name'];
+$votes = $data['votes'];
+
 // Przygotowanie zapytania SQL do wstawienia danych
 $sql = "INSERT INTO votes (name, choice, points) VALUES (?, ?, ?)";
 $stmt = $conn->prepare($sql);
 
 // Wstawianie danych do bazy
 foreach ($votes as $vote) {
-    $choice = $vote->choice;
-    $points = $vote->points;
+    $choice = $vote['choice'];
+    $points = $vote['points'];
     $stmt->bind_param("ssi", $name, $choice, $points);
     $stmt->execute();
 }
@@ -41,7 +36,7 @@ foreach ($votes as $vote) {
 $stmt->close();
 $conn->close();
 
-// Odpowiedź na zapisanie danych
+// Odpowiedź po zapisie
 $response = array("message" => "Votes saved successfully");
 echo json_encode($response);
 
